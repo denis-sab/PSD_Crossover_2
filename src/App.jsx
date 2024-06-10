1278421ae49dc7645330d6d45685b528e8e37291
Denis Sab <denis.sabs@ymail.com>
2024-06-10 10:17:39 +0200

    created react app fr crossover PSD. Added Tailwind, react-router, basic components and pages

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import {Routes, Route} from "react-router-dom"
+import Layout from "./pages/Layout"
+import Home from "./pages/Home"
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route path='/' element={<Home />} />
+        </Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Layout.jsx
@@ -0,0 +1,19 @@
+import { Outlet } from "react-router-dom"
+import Footer from "../components/Footer"
+import Navbar from "../components/Navbar"
+
+
+
+function Layout() {
+  return (
+    <>
+       <Navbar />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  )
+}
+
+export default Layout
