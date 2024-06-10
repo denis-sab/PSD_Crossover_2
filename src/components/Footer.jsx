1c7738630b6ea282bdadff71e554b161713692d6
Sebastian Weber <s-weber85@gmx.de>
2024-06-10 11:51:30 +0200

    tailwind installation fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <>
+      <div className='flex flex-row justify-between bg-red-500'>Footer
+      <p class="text-sky-400/50">The quick brown fox...</p>
+        <p className=''>Terms and conditions</p>
+        <p>Privacy</p>
+        <p>Follow</p>
+        <div className='flex flex-row h-10 bg-slate-400'></div>
+      </div>
+    </>
   )
 }
 
