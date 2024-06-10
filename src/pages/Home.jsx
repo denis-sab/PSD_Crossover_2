06f1a5e6bd66c1345f40fffec27a319141c485dc
Erdinc96 <erdinc.y96@hotmail.com>
2024-06-10 14:59:41 +0200

    added AboutUs section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import AboutUs from "../components/AboutUs"
 import Newsletter from "../components/Newsletter"
+import Hero from "../components/Hero"
 
 function Home() {
   return (
     <>
     <div>
+      <Hero/>
       <AboutUs/>
       <Newsletter />
     </div>
