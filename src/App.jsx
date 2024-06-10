e637f95b8c0f1f2e133b20fa6ab1817ab15d830b
Denis Sab <denis.sabs@ymail.com>
2024-06-10 15:37:58 +0200

    merged Footer and fixed about us pdding and Hero text

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ function App() {
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route path='/' element={<Home />} />
-          <Route path='/' element={<Footer />} />
         </Route>
       </Routes>
     </>
